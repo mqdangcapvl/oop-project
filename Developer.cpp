ec2ca9a657b6e5fdac64c9f83ac40e844d9244a3
Minh Quan <trinhminhquan203@gmail.com>
2026-04-02 11:56:18 +0700

    123

--- Developer.cpp
+++ Developer.cpp
@@ -15,6 +15,6 @@ double Developer::calculateSalary() const {
 }
 
 ostream& operator<<(ostream& os, const Developer& d) {
-    os << "ID = " << d.getId() << " Name : " << d.name << " Dept: " << d.getDepartment() << " Perf=" << d.getPerformance() << " Salary=" << d.calculateSalary();
+    os << "ID = " << d.getId() << " Name : " << d.getName() << " Dept: " << d.getDepartment() << " Perf=" << d.getPerformance() << " Salary=" << d.calculateSalary();
     return os;
 }
